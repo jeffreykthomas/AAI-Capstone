ad09fc00fecd1d6ea39e32a948f5c43951df904b
Jeffrey Thomas <jeffreykthomas@gmail.com>
2024-04-06 21:41:33 -0700

    Remove initial prompt from output in generation

--- evaluate_model/evaluate_model.py
+++ evaluate_model/evaluate_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from tqdm import tqdm
 
 df_comparison = pd.read_csv('data/datasets/RogueEval/predictions.csv')
 df_comparison = df_comparison.sample(100)
@@ -8,9 +9,9 @@ df_comparison = df_comparison.reset_index(drop=True)
 
 comparison_path = 'Qwen/Qwen1.5-0.5B-Chat'
 comparison_model = AutoModelForCausalLM.from_pretrained(comparison_path)
-comparison_tokenizer = AutoTokenizer.from_pretrained(comparison_path)
+comparison_tokenizer = AutoTokenizer.from_pretrained(comparison_path, padding_side='left')
 
-device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 comparison_model = comparison_model.to(device)
 comparison_model.eval()
 num_batches = 25
@@ -21,7 +22,6 @@ num_rows = len(df_comparison)
 def calculate_comparison_predictions(texts, batch_size=4):
     message_batch = []
     for i in range(batch_size):
-        print(f'Adding message {texts[i]}')
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": texts[i]}
@@ -40,21 +40,25 @@ def calculate_comparison_predictions(texts, batch_size=4):
     # Decode the trimmed output token IDs to text for the entire batch
     generated_texts = comparison_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
+    # Remove text from texts from the generated texts
+    for i in range(len(generated_texts)):
+        text_to_replace = texts[i].replace('<|im_start|>', '').replace('<|im_end|>', '')
+        generated_texts[i] = generated_texts[i].replace(text_to_replace, '').strip()
+
     return generated_texts
 
 
 if __name__ == '__main__':
     all_generated_texts = []
-    for batch_num in range(num_batches):
+    for batch_num in tqdm(range(num_batches)):
         # Calculate start and end index of the current batch
         print(f'Batch: {batch_num}/{num_batches}')
         start_idx = batch_num * batch_size
         end_idx = min((batch_num + 1) * batch_size, num_rows)
-        print(f'Start: {start_idx}, End: {end_idx}')
         # Extract the texts for the current batch from the DataFrame
         current_batch_texts = df_comparison['Input'][start_idx:end_idx].tolist()
 
-        # Adjust the batch size if we're on the last batch and it's smaller than the batch_size
+        # Adjust the batch size if we're on the last batch, and it's smaller than the batch_size
         current_batch_size = len(current_batch_texts)
         if current_batch_size < batch_size:
             # Update the function call to handle a smaller batch if necessary
@@ -65,6 +69,6 @@ if __name__ == '__main__':
 
         # Append the generated texts from the current batch to the all_generated_texts list
         all_generated_texts.extend(generated_texts)
-        print(f'Texts: {[text[:50] for text in generated_texts]}')
+
     df_comparison['Comparison_Predictions'] = all_generated_texts
     df_comparison.to_csv('data/datasets/RogueEval/predictions_with_comparison.csv', index=False)
